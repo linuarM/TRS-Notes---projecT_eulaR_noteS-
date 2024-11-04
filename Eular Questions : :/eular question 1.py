1d6ff41f7c0ca842755033266d77b5e2e5b16b65
linuarM <egekanatege@gmail.com>
2024-11-04 21:13:48 +0300

    Update eular question 1.py

--- Eular Questions : :/eular question 1.py
+++ Eular Questions : :/eular question 1.py
@@ -1,1 +1,13 @@
+def result(randomsomethingsoso):
 
+  for num in range(randomsomethingsoso):
+
+    if num % 3 == 0 and num % 5 == 0:
+      print(str(num) + " ", end = "")
+    else:
+      pass
+
+if __name__ == "__main__":
+  N = 1000
+
+result(randomsomethingsoso)
