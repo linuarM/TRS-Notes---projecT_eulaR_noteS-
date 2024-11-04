028a7e1279a2f48ae5e1cf8763405da9b77639b4
linuarM <egekanatege@gmail.com>
2024-11-04 21:23:53 +0300

    Update eular question 1.py

--- Eular Questions : :/eular question 1.py
+++ Eular Questions : :/eular question 1.py
@@ -1,13 +1,15 @@
-def result(randomsomethingsoso):
+def result(q,number1,number2):
 
-  for num in range(randomsomethingsoso):
+  for num in range(q):
 
-    if num % 3 == 0 and num % 5 == 0:
+    if num % number1 == 0 and num % number2 == 0:
       print(str(num) + " ", end = "")
     else:
       pass
 
 if __name__ == "__main__":
-  N = 1000
+  q = 1000
+  number1=3
+  number2=5
 
-result(randomsomethingsoso)
+  
